5137b44474886f4aaf548a5990babb64c8f41b2f
Tomas Kourim <kourim@outlook.com>
2020-11-20 21:43:43 +0100

    heparin recommendation

--- backend/heparin/heparin_dosage.py
+++ backend/heparin/heparin_dosage.py
@@ -10,7 +10,12 @@ class HeparinRecommendation:
     doctor_warning: str = None
 
 
-def calculate_recommended_dosage(weight: float, target_aptt_low: float, target_aptt_high: float, current_aptt: float,
-                                 solution_heparin_units: float, solution_ml: float) -> HeparinRecommendation:
+def recommended_heparin(weight: float, target_aptt_low: float, target_aptt_high: float, current_aptt: float,
+                        previous_aptt: float, solution_heparin_units: float,
+                        solution_ml: float) -> HeparinRecommendation:
     return 0
 
+
+def calculate_recommended_dosage(weight: float, target_aptt_low: float, target_aptt_high: float, current_aptt: float,
+                                 solution_heparin_units: float, solution_ml: float) -> float:
+    return 0
