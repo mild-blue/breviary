ee2b265d06010fa72e5931497abc79d204035080
Marek Polak <marek.polak@mild.blue>
2020-11-20 20:31:07 +0100

    add swagger

--- backend/__init__.py
+++ backend/__init__.py
@@ -7,6 +7,7 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 from backend.api.v1 import API_VERSION as V1
 from backend.api.v1.service_api import namespace as service_namespace
 from backend.api.v1.shared_models import namespace as shared_models_namespace
+from backend.api.v1.patients_api import namespace as patients_namespace
 from backend.api.v1.user_api import namespace as user_namespace
 from backend.configuration.application_configuration import ApplicationConfiguration, get_application_configuration
 from backend.common.db.database import get_db_session, init_db, migrate_db, build_db_connection_string
@@ -60,6 +61,7 @@ def create_app() -> Flask:
         )
         api.init_app(app)
         api.add_namespace(shared_models_namespace)
+        api.add_namespace(patients_namespace, path=f'{V1}/patients')
         api.add_namespace(service_namespace, path=f'{V1}/service')
         api.add_namespace(user_namespace, path=f'{V1}/user')
 
