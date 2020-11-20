7ea7423faef691a1647f0bbda9c58bbd407bf8f7
Tomas Kourim <kourim@outlook.com>
2020-11-20 21:35:38 +0100

    heparin dosage calculation

---
+++ backend/heparin/heparin_dosage.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+
+@dataclass
+class HeparinRecommendation:
+    heparin_continuous_dosage: float
+    heparin_bolus_dosage: float
+    next_remainder: datetime
+    doctor_warning: str = None
+
+
+def calculate_recommended_dosage(weight: float, target_aptt_low: float, target_aptt_high: float, current_aptt: float,
+                                 solution_heparin_units: float, solution_ml: float) -> HeparinRecommendation:
+    return 0
+
