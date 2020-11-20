ece582a6f3a8efee346ca507653be9ec2ea26b8d
Marek Polak <marazt@gmail.com>
2020-11-20 22:27:10 +0100

    Add heparin_recommendation_api.

---
+++ backend/api/v1/heparin_recommendation_api.py
@@ -0,0 +1,53 @@
+# pylint: disable=no-self-use
+# Can not, the methods here need self due to the annotations. They are used for generating swagger which needs class.
+import logging
+
+from flask import request
+from flask_restx import Resource, fields, Namespace
+
+from backend.api.v1.shared_models import failed_response
+from backend.heparin.heparin_dosage import recommended_heparin
+
+logger = logging.getLogger(__name__)
+
+# create the namespace
+namespace = Namespace('heparin-recommendation')
+
+# shared models
+heparin_recommendation_out_model = namespace.model('HeparinRecommendationOut', {
+    'heparin_continuous_dosage': fields.Float(required=True, description='Heparin countinuous dosage.'),
+    'heparin_bolus_dosage': fields.Float(required=True, description='Heparin bolus dosage.'),
+    'next_remainder': fields.DateTime(required=True, description='Next reminder.'),
+    'doctor_warning': fields.String(required=False, description='Doctor warning.'),
+})
+
+
+@namespace.route('/heparin-recommendation')
+class HeparinRecommendationApi(Resource):
+    heparin_recommendation_in_model = namespace.model('HeparinRecommendationIn', {
+        'weight': fields.Float(required=True),
+        'target_aptt_low': fields.Float(required=True),
+        'target_aptt_high': fields.Float(required=True),
+        'current_aptt': fields.Float(required=True),
+        'previous_aptt': fields.Float(required=True),
+        'solution_heparin_units': fields.Float(required=True),
+        'solution_ml': fields.Float(required=True)
+    })
+
+    @namespace.doc(body=heparin_recommendation_in_model)
+    @namespace.response(code=200, model=heparin_recommendation_out_model,
+                        description='Heparin recommendation.')
+    @namespace.response(code=400, model=failed_response, description='Wrong data format.')
+    @namespace.response(code=401, model=failed_response, description='Authentication failed.')
+    @namespace.response(code=500, model=failed_response, description='Unexpected error, see contents for details.')
+    def post(self):
+        post_data = request.get_json()
+        return recommended_heparin(
+            weight=float(post_data['weight']),
+            target_aptt_low=float(post_data['target_aptt_low']),
+            target_aptt_high=float(post_data['target_aptt_high']),
+            current_aptt=float(post_data['current_aptt']),
+            previous_aptt=float(post_data['previous_aptt']),
+            solution_heparin_units=float(post_data['solution_heparin_units']),
+            solution_ml=float(post_data['solution_ml'])
+        )
--- backend/heparin/heparin_dosage.py
+++ backend/heparin/heparin_dosage.py
@@ -10,12 +10,24 @@ class HeparinRecommendation:
     doctor_warning: str = None
 
 
-def recommended_heparin(weight: float, target_aptt_low: float, target_aptt_high: float, current_aptt: float,
-                        previous_aptt: float, solution_heparin_units: float,
-                        solution_ml: float) -> HeparinRecommendation:
+def recommended_heparin(
+        weight: float,
+        target_aptt_low: float,
+        target_aptt_high: float,
+        current_aptt: float,
+        previous_aptt: float,
+        solution_heparin_units: float,
+        solution_ml: float
+) -> HeparinRecommendation:
     return 0
 
 
-def _calculate_recommended_dosage(weight: float, target_aptt_low: float, target_aptt_high: float, current_aptt: float,
-                                 solution_heparin_units: float, solution_ml: float) -> float:
+def _calculate_recommended_dosage(
+        weight: float,
+        target_aptt_low: float,
+        target_aptt_high: float,
+        current_aptt: float,
+        solution_heparin_units: float,
+        solution_ml: float
+) -> float:
     return 0
