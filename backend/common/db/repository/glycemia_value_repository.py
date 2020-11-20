81cbdec78e40267b6c18b534441eaa270392419e
Marek Polak <marazt@gmail.com>
2020-11-20 21:57:15 +0100

    Insulin model.

--- backend/common/db/repository/glycemia_value_repository.py
+++ backend/common/db/repository/glycemia_value_repository.py
@@ -3,38 +3,38 @@ from typing import List, Optional
 from sqlalchemy.orm import Session  # pylint: disable=import-error
 
 from backend.common.db.database import get_db_session
-from backend.common.db.model.heparin_dosage import HeparinDosage
+from backend.common.db.model.glycemia_value import GlycemiaValue
 from backend.common.db.repository.base_repository import BaseRepository
 
 
-class HeparinDosageRepository(BaseRepository):
+class GlycemiaValueRepository(BaseRepository):
     """
-    HeparinDosage repository
+    GlycemiaValue repository
     """
 
     @staticmethod
-    def get_all() -> List[HeparinDosage]:
-        sup = super(HeparinDosageRepository, HeparinDosageRepository)
-        return sup.base_get_all(HeparinDosage)  # type: ignore
+    def get_all() -> List[GlycemiaValue]:
+        sup = super(GlycemiaValueRepository, GlycemiaValueRepository)
+        return sup.base_get_all(GlycemiaValue)  # type: ignore
 
     @staticmethod
-    def create(heparin_dosage: HeparinDosage, commit: bool = True) -> HeparinDosage:
-        sup = super(HeparinDosageRepository, HeparinDosageRepository)
-        return sup.base_create(heparin_dosage, commit)  # type: ignore
+    def create(glycemia_value: GlycemiaValue, commit: bool = True) -> GlycemiaValue:
+        sup = super(GlycemiaValueRepository, GlycemiaValueRepository)
+        return sup.base_create(glycemia_value, commit)  # type: ignore
 
     @staticmethod
-    def create_many(heparin_dosages: List[HeparinDosage], commit: bool = True) -> List[HeparinDosage]:
-        sup = super(HeparinDosageRepository, HeparinDosageRepository)
-        sup.base_create_many(heparin_dosages, commit)  # type: ignore
-        return heparin_dosages
+    def create_many(glycemia_values: List[GlycemiaValue], commit: bool = True) -> List[GlycemiaValue]:
+        sup = super(GlycemiaValueRepository, GlycemiaValueRepository)
+        sup.base_create_many(glycemia_values, commit)  # type: ignore
+        return glycemia_values
 
     @staticmethod
     def count(session: Optional[Session] = None) -> int:
         session = session if session else get_db_session()
         assert session is not None, "DB must be initialized at this point."
-        return session.query(HeparinDosage).count()  # type: ignore  # pylint: disable=no-member
+        return session.query(GlycemiaValue).count()  # type: ignore  # pylint: disable=no-member
 
     @staticmethod
-    def get_by_id(idd: int) -> Optional[HeparinDosage]:
-        sup = super(HeparinDosageRepository, HeparinDosageRepository)
-        return sup.base_get_by_id(HeparinDosage, idd)  # type: ignore
+    def get_by_id(idd: int) -> Optional[GlycemiaValue]:
+        sup = super(GlycemiaValueRepository, GlycemiaValueRepository)
+        return sup.base_get_by_id(GlycemiaValue, idd)  # type: ignore
